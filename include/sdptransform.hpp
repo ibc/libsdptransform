248592568ede29c1c494e81b67ab96b457ec1cff
Gavin Smith <gavin.smith@coralbay.tv>
2024-02-21 16:39:07 +0000

    Use nlohmann json as external dependency

--- include/sdptransform.hpp
+++ include/sdptransform.hpp
@@ -1,14 +1,13 @@
 #ifndef SDPTRANSFORM_HPP
 #define SDPTRANSFORM_HPP
 
-#include "json.hpp"
+#include <nlohmann/json_fwd.hpp>
+
 #include <string>
 #include <vector>
 #include <map>
 #include <regex>
 #include <functional>
-
-using json = nlohmann::json;
 
 namespace sdptransform
 {
@@ -22,23 +21,23 @@ namespace sdptransform
 			std::vector<std::string> names;
 			std::vector<char> types;
 			std::string format;
-			std::function<const std::string(const json&)> formatFunc;
+			std::function<const std::string(const nlohmann::json&)> formatFunc;
 		};
 
 		extern const std::map<char, std::vector<Rule>> rulesMap;
 	}
 
-	json parse(const std::string& sdp);
+	nlohmann::json parse(const std::string& sdp);
 
-	json parseParams(const std::string& str);
+	nlohmann::json parseParams(const std::string& str);
 
 	std::vector<int> parsePayloads(const std::string& str);
 
-	json parseImageAttributes(const std::string& str);
+	nlohmann::json parseImageAttributes(const std::string& str);
 
-	json parseSimulcastStreamList(const std::string& str);
+	nlohmann::json parseSimulcastStreamList(const std::string& str);
 
-	std::string write(json& session);
+	std::string write(const nlohmann::json& session);
 }
 
-#endif
+#endif // SDPTRANSFORM_HPP
